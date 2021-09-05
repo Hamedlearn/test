7184855059976f26912b157e2603222647343de3
hamedlearn <ghasalihamed@gmail.com>
2021-09-05 18:45:15 +0430

    new get

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/hamed', (req, res) => {
+  res.send('Hello hamed')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
